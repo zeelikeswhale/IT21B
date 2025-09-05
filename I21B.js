0b5b57c45ff2febad70121302ca85b4609559300
zeelikeswhale <zeenuttahoe@gmail.com>
2025-09-05 09:48:57 +0800

    message

---
+++ I21B.js
@@ -0,0 +1,15 @@
+console.log("hello World");
+
+num1 = 2
+num2 = 3
+
+sum = num1 + num2
+diff = num1 - num2
+prod = num1 * num2
+qou = num1 / num2
+
+
+console.log ("The sum of " +num1+ " & " +num2+ " is " +sum)
+console.log ("The difference of " +num1+ " & " +num2+ " is " +diff)
+console.log ("The product of " +num1+ " & " +num2+ " is " +prod)
+console.log ("The qoutation of " +num1+ " & " +num2+ " is " +qou)
